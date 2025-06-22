060e2d17e3503b53965911644b2aa3a163629044
Loïc <loic.hardy@neurogreen.be>
2025-06-22 23:05:26 +0200

    add select menu on top of page

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,6 +1,7 @@
 import os
 import json
-from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (QMainWindow, QTableWidget, QTableWidgetItem,
+                            QVBoxLayout, QWidget, QMenuBar, QMenu, QAction)
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QIcon, QColor
 
@@ -11,25 +12,22 @@ class ScriptScopeMainWindow(QMainWindow):
         self.resize(900, 500)
         # self.setWindowIcon(QIcon("icon.png"))  # Optionnel
 
+        # Crée la barre de menus
+        self.create_menu_bar()
+
         self.table = QTableWidget()
-        self.table.setColumnCount(7)  # 7 colonnes au lieu de 6
+        self.table.setColumnCount(7)
         self.table.setHorizontalHeaderLabels(
             ["Script Name", "PID", "CPU (%)", "CPU Total (%)", "MEM (%)", "Elapsed Time", "Command"]
         )
         self.table.setAlternatingRowColors(True)
-        self.table.setColumnWidth(0, 180)   # Script Name
-        self.table.setColumnWidth(1, 80)    # PID
-        self.table.setColumnWidth(2, 80)    # CPU (%)
-        self.table.setColumnWidth(3, 100)   # CPU Total (%)
-        self.table.setColumnWidth(4, 80)    # MEM (%)
-        self.table.setColumnWidth(5, 120)   # Elapsed Time
-        self.table.setColumnWidth(6, 280)   # Command
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.table)
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        self.table.setColumnWidth(0, 180)
+        self.table.setColumnWidth(1, 80)
+        self.table.setColumnWidth(2, 80)
+        self.table.setColumnWidth(3, 100)
+        self.table.setColumnWidth(4, 80)
+        self.table.setColumnWidth(5, 120)
+        self.table.setColumnWidth(6, 280)
 
         layout = QVBoxLayout()
         layout.addWidget(self.table)
@@ -45,6 +43,36 @@ class ScriptScopeMainWindow(QMainWindow):
         self.timer.start(1000)
 
         self.refresh_table()
+
+    def create_menu_bar(self):
+        # Crée la barre de menus
+        menu_bar = self.menuBar()
+        select_menu = menu_bar.addMenu("Select")
+
+        # Liste des colonnes et de leur index
+        self.column_visibility = {
+            "Script Name": 0,
+            "PID": 1,
+            "CPU (%)": 2,
+            "CPU Total (%)": 3,
+            "MEM (%)": 4,
+            "Elapsed Time": 5,
+            "Command": 6
+        }
+
+        # Ajoute une action pour chaque colonne (case à cocher)
+        self.column_actions = {}
+        for name, col in self.column_visibility.items():
+            action = QAction(name, self, checkable=True, checked=True)
+            action.triggered.connect(lambda checked, c=col: self.toggle_column_visibility(c, checked))
+            select_menu.addAction(action)
+            self.column_actions[col] = action
+
+    def toggle_column_visibility(self, column, visible):
+        if visible:
+            self.table.showColumn(column)
+        else:
+            self.table.hideColumn(column)
 
     def refresh_table(self):
         project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -94,4 +122,3 @@ class ScriptScopeMainWindow(QMainWindow):
             self.table.setItem(row, 4, QTableWidgetItem(str(mem)))
             self.table.setItem(row, 5, QTableWidgetItem(entry.get("etime", "")))
             self.table.setItem(row, 6, QTableWidgetItem(entry.get("cmd", "")))
-
